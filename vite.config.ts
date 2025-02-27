5a776364f4d24eb00363a4ac1a119353201553c6
HuYang <hy.ccccr@gmail.com>
2025-02-28 00:44:05 +0800

    feat: :sparkles: support vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://172.16.102.30:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
